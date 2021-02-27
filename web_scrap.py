164b876cc649b0b78bdbc4ce4387c50e7b6a8659
diandiaye <41585144+diandiaye@users.noreply.github.com>
2021-02-27 22:12:09 +0100

    Update web_scrap.py

--- web_scrap.py
+++ web_scrap.py
@@ -1,1 +1,13 @@
+# Importer la librarie bs4 et requests
+from bs4 import BeautifulSoup
 
+import requests
+# Variable permetant de demander à l'utilisateur de saisir un lien
+url = input("Entrez l'URL du Site Web dans laquelle vous voulez extraire du contenu: ")
+# Obtenir le contenu du Site Web
+r  = requests.get("http://" +url)
+data = r.text
+soup = BeautifulSoup(data)
+# Afficher le contenu du Site Web
+for link in soup.find_all('a'):
+    print(link.get('href'))
